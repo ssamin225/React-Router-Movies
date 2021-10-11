9630cf80bde63046bd678094d0288a6255f3e19b
ssamin225 <sadmansamin225@gmail.com>
2021-10-11 15:52:45 -0400

    completed mvp

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default function MovieList(props) {
   return (
@@ -11,17 +12,22 @@ export default function MovieList(props) {
 }
 
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
+  const { title, director, metascore, id } = props.movie;
+  const history = useHistory();
+
+  const routeToMovie = () => {
+    history.push(`/movies/${id}`);
+  }
 
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
+    <div className="movie-card" onClick={routeToMovie}>
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
     </div>
   );
 }
